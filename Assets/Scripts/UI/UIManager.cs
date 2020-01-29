032a5c8ac3ebcee4d99bbf34a5dbf4ad5fd767bb
Daniel Keehn <danielkeehn@bellsouth.net>
2020-01-29 03:28:21 -0500

    Added Text Mesh Pro and first piece of UI that will display the amout of spawn points

--- Assets/Scripts/UI/UIManager.cs
+++ Assets/Scripts/UI/UIManager.cs
@@ -4,6 +4,9 @@ using UnityEngine;
 
 public class UIManager : MonoBehaviour
 {
+
+    public GameObject spawnPointsAmount;
+
     // Start is called before the first frame update
     void Start()
     {
