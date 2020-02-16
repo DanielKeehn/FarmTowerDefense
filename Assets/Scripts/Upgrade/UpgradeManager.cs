1e8adaf1c233302bd37d1f0f043a2935f99ad34a
Daniel Keehn <danielkeehn@bellsouth.net>
2020-02-15 20:51:34 -0500

    Couroutine method created in upgrade manager so initializing what shows up in upgrade menu at first runs after other start methods

--- Assets/Scripts/Upgrade/UpgradeManager.cs
+++ Assets/Scripts/Upgrade/UpgradeManager.cs
@@ -9,13 +9,24 @@ public class UpgradeManager : MonoBehaviour
     // This is the current animal being displayed on the upgrade menu
     public string currentAnimal;
 
+    private IEnumerator coroutine;
+    
     // Start is called before the first frame update
     void Start()
     {
         // This gets the unlcoked animals dict from the spawn manager
          animalDict = FindObjectOfType<SpawnManager>().GetComponent<SpawnManager>().getAnimalDict();
-         currentAnimal = "Cow";
-         
+
+        // This coroutine is neccesary because the changeCurrentAnimal method cannot run until the spawn manager start method and
+        // animal manager run so the unlockedAnimal Dict can be initialized
+        coroutine = Wait(0.01f);
+        StartCoroutine(coroutine);   
+    }
+
+    private IEnumerator Wait(float waitTime)
+    {
+        yield return new WaitForSeconds(waitTime);
+        changeCurrentAnimal("Cow");
     }
 
     // Update is called once per frame
