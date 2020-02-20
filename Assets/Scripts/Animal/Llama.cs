40af3c5a8ba08718e01fa706d4f5115cffb14ae2
Lilly Zhu <yzhu608@gatech.edu>
2020-02-19 20:34:54 -0500

    Animals
    added animals

---
+++ Assets/Scripts/Animal/Llama.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// Notice this class is a child of Animal
+
+public class Llama : Animal
+{
+    public Llama()
+    {
+        name = "Llama";
+        health = 5;
+        costToSpawn = 5;
+        isUnlocked = true;
+    }
+}
