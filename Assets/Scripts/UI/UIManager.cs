e76cd43d53149ef360e93546b8e2c9db57ce7a29
stephanieluo18 <55214091+stephanieluo18@users.noreply.github.com>
2020-02-20 17:30:55 -0500

    practice ( hello world)

--- Assets/Scripts/UI/UIManager.cs
+++ Assets/Scripts/UI/UIManager.cs
@@ -24,7 +24,7 @@ public class UIManager : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        Debug.Log("Hello World!");
     }
 
     public void changeSpawnPointsAmountUI(int newNum){
