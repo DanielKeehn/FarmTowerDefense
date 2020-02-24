1e99a5631c04d7d268c094c5cd72b302b4a94774
Daniel Keehn <danielkeehn@bellsouth.net>
2020-02-23 23:20:38 -0500

    The player can now attack an enemy

---
+++ Assets/Scripts/Player/PlayerAttack.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerAttack : MonoBehaviour
+{
+
+    public Camera camera;
+    public Weapon weaponScript;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        weaponScript = gameObject.GetComponent<Weapon>();
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetButtonDown("Attack")) {
+            DoAttack();
+        }
+    }
+
+    // This method runs when a player attacks
+    private void DoAttack(){
+        Ray ray = camera.ScreenPointToRay(Input.mousePosition);
+        RaycastHit hit;
+
+        if (Physics.Raycast(ray, out hit, weaponScript.attackRange)) {
+            if (hit.collider.tag == "Enemy") {
+                Enemy enemy = hit.collider.GetComponent<Enemy>();
+                enemy.TakeDamage(weaponScript.attackDamage);
+            }
+        }
+    }
+}
