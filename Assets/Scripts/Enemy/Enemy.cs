7f857323575b00ab82989d88d245626eda8ae14c
Daniel Keehn <danielkeehn@bellsouth.net>
2020-02-23 22:52:43 -0500

    Enemy class exists and enemy has method where enemies health can be lost

---
+++ Assets/Scripts/Enemy/Enemy.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    // These are the varaibles all enemies have
+    public string name;
+    public int health;
+
+    // This is the contructor
+    public Enemy(string n, int h) {
+        this.name = n;
+        this.health = h;
+    }
+
+
+    // This is the contructor when no variables are given
+    public Enemy() {
+        this.name = "No Name";
+        this.health = 100;
+
+    }
+
+    public void TakeDamage(int healthLost) {
+        this.health -= healthLost;
+        if (this.health <= 0) {
+            Debug.Log(this.name + " has died");
+        }
+        Debug.Log(this.name + " took " + healthLost + " damage");
+    }
+
+
+}
