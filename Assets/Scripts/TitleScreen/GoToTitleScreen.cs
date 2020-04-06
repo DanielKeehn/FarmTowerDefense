a487e6f6d3a235487e60a48ace2ce7cd037e92b3
Daniel Keehn <danielkeehn@bellsouth.net>
2020-04-06 15:25:46 -0400

    Cutscene transfers to title screen

---
+++ Assets/Scripts/TitleScreen/GoToTitleScreen.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Playables;
+
+public class GoToTitleScreen : MonoBehaviour
+{
+
+    public PlayableDirector playableDirector;
+    public Camera cutsceneCamera;
+    public Camera titleScreenCamera;
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Checks if the cutscene is over
+        if (playableDirector.time >= 45.0) {
+            // Deactivates cutscene camera and activates title screen camera here
+            titleScreenCamera.gameObject.SetActive(true);
+            cutsceneCamera.gameObject.SetActive(false);
+        }       
+    }
+}
