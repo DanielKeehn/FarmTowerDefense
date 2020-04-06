36c8a266888b43d43a08a17f44ba098d81d4971c
Daniel Keehn <danielkeehn@bellsouth.net>
2020-04-06 16:05:34 -0400

    The start button actually starts game now

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class GameManager : MonoBehaviour
 {
@@ -118,6 +119,10 @@ public class GameManager : MonoBehaviour
 
     public int getCurrentState() {
         return currState;
+    }
+
+    public void switchToMainGame() {
+         SceneManager.LoadScene("MainScene");
     }
 
 }
