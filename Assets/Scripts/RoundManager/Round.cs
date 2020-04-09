e298c6dd8d1a7cf42a9eadcfca40f5846879a811
Daniel Keehn <danielkeehn@bellsouth.net>
2020-04-09 16:35:00 -0400

    A round manager and round system exists

--- Assets/Scripts/RoundManager/Round.cs
+++ Assets/Scripts/RoundManager/Round.cs
@@ -4,8 +4,23 @@ using UnityEngine;
 
 public class Round : MonoBehaviour
 {
+    
+    // The number this round will appear
+    public int roundNumber;
     //Variables for a round
-    int numberOfEnemies;
+    public int numberOfEnemies;
     // This can be changed to a Random value
-    int spawnSpeed;
+    public int spawnSpeed;
+
+    public Round(int ne, int ss, int rn) {
+        this.numberOfEnemies = ne;
+        this.spawnSpeed = ss;
+        this.roundNumber = rn;
+    }
+
+    public Round() {
+        this.numberOfEnemies = 0;
+        this.spawnSpeed = 0;
+        this.roundNumber = 0;
+    }
 }
