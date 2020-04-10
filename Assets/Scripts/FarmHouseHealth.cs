0b66d665243689149fe2cc7fc60bc786178271f0
Daniel Keehn <danielkeehn@bellsouth.net>
2020-04-10 03:52:10 -0400

    Game restarts when farm loses all health

--- Assets/Scripts/FarmHouseHealth.cs
+++ Assets/Scripts/FarmHouseHealth.cs
@@ -26,6 +26,19 @@ public class FarmHouseHealth : MonoBehaviour
     // The barn house loses health every time this method is called
      public void TakeDamage(int damage) {
         currentHealth -= damage;
+        if (checkLoseState()) {
+            Debug.Log("The barn was destroyed");
+            GameManager gameManager = GameObject.Find("GameManager").GetComponent<GameManager>();
+            gameManager.switchToMainGame();
+        }
         healthbar.SetHealth(currentHealth);
+    }
+
+    public bool checkLoseState() {
+        if (currentHealth <= 0) {
+            return true;
+        } else {
+            return false;
+        }
     }
 }
