85d2f05723026f651abab7688c21dccfd613f4d3
Daniel Keehn <danielkeehn@bellsouth.net>
2020-08-08 16:52:36 -0400

    Added sound effects and sound types for apple

--- Assets/Scripts/Audio/AudioType.cs
+++ Assets/Scripts/Audio/AudioType.cs
@@ -5,6 +5,19 @@
         None,
         ST_01,
         SFX_01,
-        SFX_02
+        SFX_02,
+        SFX_Apple_Spawn,
+        SFX_Apple_Death,
+        SFX_Apple_Attack,
+        SFX_Apple_TakeDamage,
+        SFX_Apple_Walk,
+        SFX_Broccoli,
+        SFX_Carrot,
+        SFX_Corn,
+        SFX_Lettuce,
+        SFX_Onion,
+        SFX_Potato,
+        SFX_Turnip,
+        SFX_Wheat
     }
 } 
